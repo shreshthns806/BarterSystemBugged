df15d19e21f99e4e6dc2e97ffdf892cead06b8f0
shreshthns806 <shreshthns.425@gmail.com>
2020-08-24 16:47:41 +0530

    added Modal

--- screens/LoginSignup.js
+++ screens/LoginSignup.js
@@ -1,20 +1,28 @@
 import * as React from 'react';
 import * as firebase from 'firebase';
 import db from '../config'
-import { StyleSheet, Text, View, TextInput , Image, TouchableOpacity, Alert, KeyboardAvoidingView} from 'react-native';
+import { StyleSheet, Text, View, Modal, ScrollView, TextInput , Image, TouchableOpacity, Alert, KeyboardAvoidingView} from 'react-native';
 import { Header } from 'react-native-elements';
 class LoginSignupScreen extends React.Component {
-    
     constructor(){
       super();
       this.state = {
         emailID:'',
         password:'',
+        isModalVisible:false,
+        confirmPassword:'',
+        username:'',
+        mobileNumber:'',
+        address:'',
+        firstName:'',
+        lastName:'',
       }
     }
 
-    componentDidMount(){
-        this.resetState
+    isModalVisible = ()=>{
+        this.setState({
+            isModalVisible:false,
+        })
     }
 
     resetState = ()=> {
@@ -24,23 +32,183 @@ class LoginSignupScreen extends React.Component {
         })
     }
 
+    showModal = ()=> {
+        return(
+            <Modal
+                animationType='fade'
+                transparent = {false}
+                visible = {this.state.isModalVisible}
+            >
+                <View style = {styles.modalContainer}>
+                    <ScrollView>
+                        <KeyboardAvoidingView>
+                            <Text style = {styles.modalTitle}>Signup Form</Text>
+                            <Text style = {{color:'#eeeeee', textAlign:'center', marginTop:10}}>Already a user?</Text>
+                            <TouchableOpacity
+                                style = {styles.signupButton}
+                                onPress={this.isModalVisible}
+                            >
+                                <Text style = {styles.signupButtonText}>Login</Text>
+
+                            </TouchableOpacity>
+                            <TextInput
+                                style={styles.textInput1}
+                                keyboardType='email-address'
+                                placeholder='Email ID'
+                                onChangeText={
+                                    (text)=>{
+                                        this.setState({
+                                            emailID:text,
+                                        })
+                                    }
+                                }
+                                value = {this.state.emailID} 
+                            ></TextInput>
+                            <TextInput
+                                style={styles.textInput1}
+                                placeholder='Password'
+                                secureTextEntry = {true}
+                                onChangeText={
+                                    (text)=>{
+                                        this.setState({
+                                            password:text,
+                                        })
+                                    }
+                                }
+                                
+                                value = {this.state.password} 
+                            ></TextInput>
+                            <TextInput
+                                style={styles.textInput1}
+                                placeholder='Confirm Password'
+                                secureTextEntry = {true}
+                                onChangeText={
+                                    (text)=>{
+                                        this.setState({
+                                            confirmPassword:text,
+                                        })
+                                    }
+                                }
+                                value = {this.state.confirmPassword} 
+                            ></TextInput>
+                            <TextInput
+                                placeholder = 'Contact Number'
+                                style = {styles.textInput1}
+                                keyboardType='numeric'
+                                onChangeText={
+                                    (text)=>{
+                                        this.setState({mobileNumber:text})
+                                    }
+                                }
+                                maxLength={10}
+                                value = {this.state.mobileNumber}
+                            ></TextInput>
+                            <TextInput
+                                placeholder = 'Username'
+                                style = {styles.textInput1}
+                                onChangeText={
+                                    (text)=>{
+                                        this.setState({username:text})
+                                    }
+                                }
+                                maxLength={15}
+                                value = {this.state.username}
+                            ></TextInput>
+                            <TextInput
+                                placeholder = 'First Name'
+                                style = {styles.textInput1}
+                                onChangeText={
+                                    (text)=>{
+                                        this.setState({firstName:text})
+                                    }
+                                }
+                                maxLength={15}
+                                value = {this.state.firstName}
+                            ></TextInput>
+                            <TextInput
+                                placeholder = 'Last Name'
+                                style = {styles.textInput1}
+                                onChangeText={
+                                    (text)=>{
+                                        this.setState({lastName:text})
+                                    }
+                                }
+                                maxLength={15}
+                                value = {this.state.lastName}
+                            ></TextInput>
+                            <TextInput
+                                placeholder = 'Address'
+                                style = {styles.textInput1}
+                                onChangeText={
+                                    (text)=>{
+                                        this.setState({address:text})
+                                    }
+                                }
+                                value = {this.state.address}
+                            ></TextInput>
+                            <TouchableOpacity
+                                style = {[styles.signupButton, {marginTop:20,marginBottom:20,}]}
+                                onPress={this.signup}
+                            >
+                                <Text style = {[styles.signupButtonText,]}>Sign me up!</Text>
+
+                            </TouchableOpacity>
+                        </KeyboardAvoidingView>
+                    </ScrollView>
+                </View>
+            </Modal>
+        )
+    }
+
+    setAllState = ()=> {
+        this.setState({
+            emailID:'',
+            password:'',
+            confirmPassword:'',
+            username:'',
+            mobileNumber:'',
+            address:'',
+            firstName:'',
+            lastName:'',
+        })
+    }
+
     signup = async ()=> {
         const email = this.state.emailID;
         const password = this.state.password;
-        if(email && password){
+        const confirmPassword = this.state.confirmPassword;
+        const address = this.state.address;
+        const mobile = this.state.mobileNumber;
+        const username = this.state.username;
+        const firstname = this.state.firstName;
+        const lastname = this.state.lastName;
+        if((email && password && confirmPassword && address && mobile && username && firstname && lastname)&&password==confirmPassword){
             try{
                 const response = await firebase.auth().createUserWithEmailAndPassword(email,password)
                 if(response){
+                    db.collection('users').add({
+                        'email' : email,
+                        'password' : password,
+                        'address' : address,
+                        'contact' : mobile,
+                        'username' : username,
+                        'lastname' : lastname
+                    })
                     Alert.alert('User added succesfully')
                 }
             }
             catch(error){
                 Alert.alert(error.message)
+                
             }
 
         }
+        else if(password != confirmPassword){
+            Alert.alert("Password doesn't match the confirmed password")
+            this.setState({password:'', confirmPassword:''})
+        }
         else {
-            Alert.alert('Please enter Email ID and Password')
+            Alert.alert('Please fill all the entries')
         }
     }
 
@@ -87,13 +255,15 @@ class LoginSignupScreen extends React.Component {
     render(){
       return(
         <KeyboardAvoidingView style = {styles.container}>
+        {this.showModal()}
         <Header
             backgroundColor={'#32e0c4'}
             centerComponent={{
-              text: 'Barter System',
+              text: 'Goods Sale',
               style: { color: '#222831', fontSize: 20 },
             }}
         />
+        <Text style = {{textAlign:'center', fontSize:12, padding:15, color:'white',}}>Good Sale for Goods</Text>
         <TextInput
             style={styles.textInput}
             keyboardType='email-address'
@@ -120,7 +290,7 @@ class LoginSignupScreen extends React.Component {
                     console.log(this.state.emailID)
                 }
             }
-            value = {this.state.emailID} 
+            value = {this.state.password} 
         ></TextInput>
         <TouchableOpacity
            style={styles.button}
@@ -137,12 +307,14 @@ class LoginSignupScreen extends React.Component {
                 Login
             </Text>
         </TouchableOpacity>
+        <Text style = {{textAlign:'center', marginTop:25,color:'#eeeeee'}}>Not a user?</Text>
         <TouchableOpacity
-            style={styles.button}
+            style={styles.button1}
             onPress={
                 ()=>{
-                    this.signup();
-                    this.reset();
+                    this.setState({
+                        isModalVisible:true,
+                    })
                 }
             }
         >
@@ -178,10 +350,33 @@ const styles = StyleSheet.create({
         width:300,
         color:"#eeeeee",
     },
+    textInput1:{
+        marginTop:20,
+        padding:10,
+        alignSelf:'center',
+        borderWidth:5,
+        borderColor:'#32e0c4',
+        width:250,
+        color:"#eeeeee",
+    },
     button:{
         backgroundColor:'#393e46',
         width:250,
         marginTop:25,
+        alignSelf:'center',
+        height:40,
+    },
+    button1:{
+        backgroundColor:'#393e46',
+        width:250,
+        marginTop:5,
+        alignSelf:'center',
+        height:40,
+    },
+    signupButton:{
+        backgroundColor:'#222831',
+        width:100,
+        marginTop:5,
         alignSelf:'center',
         height:40,
     },
@@ -191,8 +386,27 @@ const styles = StyleSheet.create({
         alignSelf:'center',
     },
     text:{
-        color:'#222831'
-    }
+        color:'#eeeeee'
+    },
+    modalContainer: {
+        flex:1,
+        backgroundColor:'#393e46',
+        alignSelf:'center',
+        width:'100%'
+    },
+    modalTitle:{
+        backgroundColor:'#222831',
+        color:'#32e0c4',
+        fontSize:23,
+        padding:5,
+        alignContent:'center',
+        textAlign:'center',
+    },
+    signupButtonText:{
+        padding:10,
+        color:'#32e0c4',
+        alignSelf:'center',
+    },
 })
 
 export default LoginSignupScreen
